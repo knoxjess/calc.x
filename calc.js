af204caef8aca7d134f6a56b49333350b1562eea
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-06-10 12:22:32 -0500

    vars.js

---
+++ calc.js
@@ -0,0 +1,17 @@
+var operators = ["+", "-", "/", "*"];
+
+var box = null;
+var last_operation_history = null;
+var operator = null;
+var equal = null;
+var dot = null;
+
+var firstNum = true;
+
+var numbers = [];
+var operator_value;
+var last_button;
+var calc_operator;
+
+var total;
+
